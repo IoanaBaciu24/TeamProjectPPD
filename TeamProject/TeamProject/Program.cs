ea33190155c456f7bc0064f160366b7412027da8
unknown <ioana.iris@gmail.com>
2021-01-11 14:50:04 +0200

    wrapped up threaded version

--- TeamProject/TeamProject/Program.cs
+++ TeamProject/TeamProject/Program.cs
@@ -8,41 +8,20 @@ namespace TeamProject
 {
     class Program
     {
-        List<KeyValuePair<String, int[]>> pictures = new List<KeyValuePair<string, int[]>>()
+        static List<KeyValuePair<String, int[]>> pictures = new List<KeyValuePair<string, int[]>>()
         {
-            new KeyValuePair<string, int[]>("E:\\algoritzm\\pentagon.png", new int[2]{ 100, 20}),
-            new KeyValuePair<string, int[]>("E:\\algoritzm\\pentagon.png", new int[2]{ 100, 20}),//todo put the right names
-            new KeyValuePair<string, int[]>("E:\\algoritzm\\pentagon.png", new int[2]{ 100, 20}),
-            new KeyValuePair<string, int[]>("C:\\Users\\papuci\\Documents\\PPD\\TeamProj\\TeamProjectPPD\\chisi_new.jpg", new int[2]{ 100, 175}),
+            new KeyValuePair<string, int[]>("E:\\algoritzm\\pentagon.png", new int[2]{ 100, 50}),
+            new KeyValuePair<string, int[]>("C:\\Users\\papuci\\Documents\\PPD\\TeamProj\\TeamProjectPPD\\grid2_smol.jpg", new int[2]{ 100, 100}),//todo put the right names
+            new KeyValuePair<string, int[]>("C:\\Users\\papuci\\Documents\\PPD\\TeamProj\\TeamProjectPPD\\grid_rotated_smol.jpg", new int[2]{ 100, 100}),
+            new KeyValuePair<string, int[]>("C:\\Users\\papuci\\Documents\\PPD\\TeamProj\\TeamProjectPPD\\chisi_new.jpg", new int[2]{ 100, 300}),
         };
-        int NR_THREADS = 6;
+        static int NR_THREADS = 6;
 
         static void Main(string[] args)
         {
 
-            //PhotoHelper.ImRead("C:\\Users\\papuci\\Documents\\PPD\\TeamProj\\TeamProjectPPD\\chisi_new.jpg", out var width, out var height, out var buffer);
-            //PhotoHelper.ConvertImageToGreyScaleAndTresholding(width, height, 200, buffer);
-            //PhotoHelper.ImWrite("C:\\Users\\papuci\\Documents\\PPD\\TeamProj\\TeamProjectPPD\\chisi_new_ts.jpg", width, height, buffer);
 
-            PhotoHelper.ImRead("C:\\Users\\papuci\\Documents\\PPD\\TeamProj\\TeamProjectPPD\\chisi_new_ts.jpg", out var width, out var height, out var buffer);
-            //PhotoHelper.ImRead("E:\\algoritzm\\pentagon.png", out var width, out var height, out var buffer);
-
-            Console.WriteLine("HEIGHT: {0}, WIDTH: {1}", height, width);
-            Console.WriteLine("READ THE IMAGE");
-            PhotoHelper.HoughTransformThreads(width, height, buffer, 8);
-            //var HS = PhotoHelper.HoughTransform2(width, height, buffer);
-            Console.WriteLine("Done Hough Transform");
-            //var lines = PhotoHelper.GetLinesFromHS(HS, width, height, buffer, 20);
-
-            var lines = PhotoHelper.CreateLinesFromHoughSpace(PhotoHelper.H, 175);
-            Console.WriteLine(lines.Count);
-            Console.WriteLine("Created Lines from HS");
-            PhotoHelper.AddLinesToPhoto(lines, width, height, buffer);
-            Console.WriteLine("Added lines");
-            PhotoHelper.ImWrite("C:\\Users\\papuci\\Documents\\PPD\\TeamProj\\TeamProjectPPD\\result6.png", width, height, buffer);
-            Console.WriteLine("Done!!");
-
-
+            RunConsole();
 
 
 
@@ -69,9 +48,25 @@ namespace TeamProject
                 switch(cmd)
                 {
                     case "1":
-                        Console.WriteLine();
+                        Console.WriteLine("creating pentagon");
+                        ExecuteTransform(pictures[0].Key, pictures[0].Value[0], pictures[0].Value[1], 1);
                         break;
+                    case "2":
+                        Console.WriteLine("creating grid");
+                        ExecuteTransform(pictures[1].Key, pictures[1].Value[0], pictures[1].Value[1], 2);
+                        break;
+                    case "3":
+                        Console.WriteLine("creating rotated grid");
+                        ExecuteTransform(pictures[2].Key, pictures[2].Value[0], pictures[2].Value[1], 3);
+                        break;
+                    case "4":
+                        Console.WriteLine("creating cat");
+                        ExecuteTransform(pictures[3].Key, pictures[3].Value[0], pictures[3].Value[1], 4);
+                        break;
+                    case "0":
+                        return;
                     default:
+                      
                         break;
                 }
 
@@ -82,10 +77,12 @@ namespace TeamProject
         {
             PhotoHelper.ImRead(path, out var width, out var height, out var buffer);
             PhotoHelper.ConvertImageToGreyScaleAndTresholding(width, height, bwTreshold, buffer);
+            PhotoHelper.ImWrite("C:\\Users\\papuci\\Documents\\PPD\\TeamProj\\TeamProjectPPD\\grey_grid.png", width, height, buffer);
+
             Console.WriteLine("CONVERTED TO BINARY IMAGE, WIDTH: {0}, HEIGHT: {1}", width, height);
-            PhotoHelper.HoughTransformThreads(width, height, buffer, 8);
+            PhotoHelper.HoughTransformThreads(width, height, buffer, NR_THREADS);
             Console.WriteLine("Done Hough Transform");
-            var lines = PhotoHelper.CreateLinesFromHoughSpace(PhotoHelper.H, 175);
+            var lines = PhotoHelper.CreateLinesFromHoughSpace(PhotoHelper.H, htTreshold);
             Console.WriteLine(lines.Count);
             Console.WriteLine("Created Lines from HS");
             PhotoHelper.AddLinesToPhoto(lines, width, height, buffer);
